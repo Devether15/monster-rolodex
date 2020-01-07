b07e6427fec10bb8fd170d4fe32e007d1581f9b7
jose <josaemoya.inf@gmail.com>
2020-01-07 10:40:21 -0300

    Updates

---
+++ precache-manifest.da80b25b40bf40c97131f4b9543fa7d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab5a72fecaa3a3da94d0967761721887",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a7ad76923aaeeb12776c",
+    "url": "/monsters-rolodex/static/css/main.59bc9beb.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a7ad76923aaeeb12776c",
+    "url": "/monsters-rolodex/static/js/main.bd3a1eab.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
